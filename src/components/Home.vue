9bb17cdb44d17a62ea8de168e504dc37703bbd0d
p1k2i <pro100kryto.original@gmail.com>
2024-03-13 01:37:05 +0000

    Change header

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,12 @@
 </script>
 
 <template>
-  <h1>Home</h1>
+  <header class="site-header">
+    <div class="container-fluid p-3 mb-2 bg-primary text-white">
+      <p class="text-center">Nikolay Mironov (p1k2i)</p>
+      <h1 class="text-center">Professional Full-Stack Developer</h1>
+    </div>
+  </header>
 </template>
 
 <style scoped>
