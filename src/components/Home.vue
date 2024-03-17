dd2f94af7356869f741f6456158d6901513e6924
p1k2i <pro100kryto.original@gmail.com>
2024-03-17 21:07:41 +0000

    Add header size adaptation

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,8 @@
   <header class="site-header">
     <div class="container-fluid p-3 mb-2 bg-primary text-white">
       <p class="text-center">Nikolay Mironov (p1k2i)</p>
-      <h1 class="text-center">Professional Full-Stack Developer</h1>
+      <h1 class="text-center d-none d-md-block">Professional Full-Stack Developer</h1>
+      <h2 class="text-center d-md-none">Professional Full-Stack Developer</h2>
     </div>
   </header>
 </template>
