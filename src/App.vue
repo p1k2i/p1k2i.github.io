87cad1bfc292dd8d5503bafc5bd496d441d7532d
p1k2i <pro100kryto.original@gmail.com>
2024-03-21 01:08:39 +0000

    Create TheFooter component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <script setup lang="ts">
 import HomePage from './components/pages/HomePage.vue';
 import TheHeader from './components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
 </script>
 
 <template>
   <TheHeader />
   <HomePage />
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <TheFooter />
 </template>
 
 <style lang="css" src="./styles/App.css" scoped></style>
